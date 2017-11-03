c20de8ffb8888a4242a21be5acc468069552d942
Ghaidairi <Ghaidairi@hotmail.com>
2017-11-03 19:33:00 +0300

    comm

--- js/clean.js
+++ js/clean.js
@@ -9,13 +9,13 @@ function Place(name) {
     this.marker = marker;
 }
 
+// viewModel
 var ViewModel = function() {
     var self = this;
     this.names = ko.observableArray(markers);
     self.openWindow = function(location) {
         google.maps.event.trigger(location.marker, 'click');
     };
-
     this.myInput = ko.observable('');
 
     // search function(value) {
@@ -31,71 +31,32 @@ var ViewModel = function() {
     this.filteredList = ko.computed(function() {
         var filter = this.myInput().toLowerCase();
         //console.log(filter);
-
-
         return ko.utils.arrayFilter(this.names(), function(item) {
             var result = (item.name.toLowerCase().indexOf(filter) !== -1);
             item.marker.setVisible(result);
             return result;
         });
-
-        // for() {
-        //   marker.setMap(null);
-        // }
-    }, this);
-
-
+    }, this);// end of computed
     // this.myInput = ko.observable('');
     // console.log(this.myInput());
     // this.runWhenSomethingChanges = ko.computed(function() {
     //   console.log(this.myInput());
     //   this.search(this.myInput());
 
-    // https://opensoul.org/2011/06/23/live-search-with-knockoutjs/
-    // Filtering an Array/ ko.utils.arrayFilter:
-    // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
-    // to search for sub strings https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
-    // to search for lower case https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
-
     // ajax request to get info for current location
-     var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + this.name + '&format=json&callback=wikiCallback';
+     var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + this.names + '&format=json&callback=wikiCallback';
      var content;
+     var title = markers.name;
+     var wikiLinks;
      $.ajax({
          url: wikiURL,
          dataType: 'jsonp',
          timeout: 1000
      }).done(function(data) {
-         this.content += '<h3>' + this.name + '</h3>' + '<p>' + data[2][0] + '<a href=' + data[3][0] + ' target="blank"> Wikipedia</a></p>';
+         this.content += '<h3>' + this.names + '</h3>' + '<p>' + data[2][0] + '<a href=' + data[3][0] + ' target="blank"> Wikipedia</a></p>';
      }).fail(function(jqXHR, textStatus) {
          alert("failed to get wikipedia resources");
      });
-
-     // A function to parse and display the Wikipedia info.
-     var parseAjax = function(response) {
-    // // Removing the temporary span in the View
-    // if ($('#tempText').is(":visible")) $('#tempText').hide();
-    // // Hiding the old Wikipedia info.
-    // $('#wikipedia').hide();
-    // Checking if the AJAX has a response
-    if (response === "failed") {
-      // If it failed, display an error message
-      self.title("Cannot load Wikipedia data!");
-    // If the AJAX is successful
-    } else {
-      // Displaying Wikipedia info. title
-      self.title(response[0]);
-      // Displaying Wikipedia info. synopsis (if any)
-      self.content(response[2].length > 0 ? response[2] : 'No Description!');
-      // Displaying Wikipedia info. links (if any)
-      self.wikiLinks([]);
-      response[1].forEach(function (link) {
-        self.wikiLinks.push( {'link': link, 'url': 'https://en.wikipedia.org/wiki/' + link} );
-      });
-    }
-    // Displaying the Wikipedia info. slowly
-    $('#wikipedia').show('slow');
-  };
-
 };
 // end of viewModel
 
@@ -161,6 +122,7 @@ var markers = [{
     }
 ];
 
+// initialiing google map
 function initMap() {
     // Map options
     var options = {
@@ -190,18 +152,13 @@ function initMap() {
             position: props.coords,
             map: map
         });
-
-
         ///this.names()[i].marker = marker;
 
         // Check content
-
         if (props.content) {
-
             var infoWindow = new google.maps.InfoWindow({
                 content: props.content
             });
-
             marker.addListener('click', function() {
                 var marker = this;
                 infoWindow.open(map, marker);
@@ -210,9 +167,7 @@ function initMap() {
                     marker.setAnimation(null);
                 }, 1400);
             });
-
             props.marker = marker;
         } // if stat. closing
     } // addMarker func. closing
-
 } // initMap func. closing
