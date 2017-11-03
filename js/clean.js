81bc468c6bbe991af4c175578e1db41f9ae76ed9
Ghaidairi <Ghaidairi@hotmail.com>
2017-11-03 16:54:45 +0300

    comm

--- js/clean.js
+++ js/clean.js
@@ -51,26 +51,64 @@ var ViewModel = function() {
     //   console.log(this.myInput());
     //   this.search(this.myInput());
 
-
     // https://opensoul.org/2011/06/23/live-search-with-knockoutjs/
     // Filtering an Array/ ko.utils.arrayFilter:
     // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
     // to search for sub strings https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
     // to search for lower case https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
 
+    // ajax request to get info for current location
+     var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + this.name + '&format=json&callback=wikiCallback';
+     var content;
+     $.ajax({
+         url: wikiURL,
+         dataType: 'jsonp',
+         timeout: 1000
+     }).done(function(data) {
+         this.content += '<h3>' + this.name + '</h3>' + '<p>' + data[2][0] + '<a href=' + data[3][0] + ' target="blank"> Wikipedia</a></p>';
+     }).fail(function(jqXHR, textStatus) {
+         alert("failed to get wikipedia resources");
+     });
+
+     // A function to parse and display the Wikipedia info.
+     var parseAjax = function(response) {
+    // // Removing the temporary span in the View
+    // if ($('#tempText').is(":visible")) $('#tempText').hide();
+    // // Hiding the old Wikipedia info.
+    // $('#wikipedia').hide();
+    // Checking if the AJAX has a response
+    if (response === "failed") {
+      // If it failed, display an error message
+      self.title("Cannot load Wikipedia data!");
+    // If the AJAX is successful
+    } else {
+      // Displaying Wikipedia info. title
+      self.title(response[0]);
+      // Displaying Wikipedia info. synopsis (if any)
+      self.content(response[2].length > 0 ? response[2] : 'No Description!');
+      // Displaying Wikipedia info. links (if any)
+      self.wikiLinks([]);
+      response[1].forEach(function (link) {
+        self.wikiLinks.push( {'link': link, 'url': 'https://en.wikipedia.org/wiki/' + link} );
+      });
+    }
+    // Displaying the Wikipedia info. slowly
+    $('#wikipedia').show('slow');
+  };
 
 };
-
+// end of viewModel
 
 
 var map;
+var wikiLink;
 // Array of markers
 var markers = [{
         coords: {
             lat: 25.3431103,
             lng: 49.6135413
         },
-        content: '<h1>Panda</h1><h4>Supermarket</h4>',
+        content: '<h4>Panda</h4><h5>Supermarket</h5>',
         name: 'Panda'
     },
     {
@@ -78,7 +116,7 @@ var markers = [{
             lat: 25.3420998,
             lng: 49.6139416
         },
-        content: '<h1>Extra</h1>',
+        content: '<h4>Extra</h4>',
         name: 'Extra'
     },
     {
@@ -86,7 +124,7 @@ var markers = [{
             lat: 25.3340100,
             lng: 49.6149448
         },
-        content: '<h1>Sommar Caffee</h1>',
+        content: '<h4>Sommar Caffee</h4>',
         name: 'Sommer Caffee'
     },
     {
@@ -94,7 +132,7 @@ var markers = [{
             lat: 25.3344367,
             lng: 49.6131729
         },
-        content: '<h1>Little Wishes</h1>',
+        content: '<h4>Little Wishes</h4>',
         name: 'Little Wishes'
     },
     {
@@ -102,7 +140,7 @@ var markers = [{
             lat: 25.3278104,
             lng: 49.6152120
         },
-        content: '<h1>Code Caffee and Resturant</h1>',
+        content: '<h4>Code Caffee and Resturant</h4>',
         name: 'Code Caffee and Resturant'
     },
     {
@@ -110,7 +148,7 @@ var markers = [{
             lat: 25.3350340,
             lng: 49.6137941
         },
-        content: '<h1>iPhone Center</h1>',
+        content: '<h4>iPhone Center</h4>',
         name: 'iPhone Center'
     },
     {
@@ -118,7 +156,7 @@ var markers = [{
             lat: 25.3315518,
             lng: 49.6151993
         },
-        content: '<h1>Jarir Bookstore</h1>',
+        content: '<h4>Jarir Bookstore</h4>',
         name: 'Jarir Bookstore'
     }
 ];
@@ -155,20 +193,6 @@ function initMap() {
 
 
         ///this.names()[i].marker = marker;
-
-
-        // ajax request saeed
-        var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.name + '&format=json&callback=wikiCallback';
-
-        $.ajax({
-            url: wikiURL + self.myInput,
-            dataType: 'jsonp',
-            timeout: 1000
-        }).done(function(data) {
-            self.content = '<h3>' + self.name() + '</h3>' + '<p>' + data[2][0] + '<a href=' + data[3][0] + ' target="blank"> Wikipedia</a></p>';
-        }).fail(function(jqXHR, textStatus) {
-            alert("failed to get wikipedia resources");
-        });
 
         // Check content
 
